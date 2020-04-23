3f5802eb7a3288f72e668b3a3fff19fc6ec4a5bc
Rob W <robpwatkins@gmail.com>
2020-04-23 17:59:51 -0500

    omg finally figured it out

--- src/components/Import.js
+++ src/components/Import.js
@@ -1,10 +1,37 @@
 import React from 'react'
+import { Button, Table, TableHead, TableRow, TableCell, TableBody, Container } from '@material-ui/core'
 
 const Import = (props) => {
-    // fill out this component
 
     return (
-        <p>Import Component</p>
+        <Container style={{textAlign: "center"}}>
+            <Button
+                onClick={props.fetchMakes}
+                variant="contained"
+                color="primary"
+                style={{marginTop: "25px"}}
+                >Click ME!
+            </Button>
+            <Table>
+                <TableHead>
+                    <TableRow>
+                        <TableCell>Id</TableCell>
+                        <TableCell>Make</TableCell>
+                        <TableCell>Action</TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {props.makes.map((make, index) => {
+                        return (
+                            <TableRow key={index}>
+                                <TableCell>{make.MakeId}</TableCell>
+                                <TableCell>{make.MakeName}</TableCell>
+                            </TableRow>
+                        )
+                    })}
+                </TableBody>
+            </Table>
+        </Container>
     )
 }
 
--- src/redux/actions.js
+++ src/redux/actions.js
@@ -13,3 +13,20 @@ export const removeCar = (index) => {
         value: index
     }
 }
+
+export const fetchMakes = () => {
+    return (dispatch) => {
+        fetch(url)
+            .then(res => res.json())
+            .then(response => {
+                // console.log(response.Results.map(make => make.MakeName))
+                const action = {
+                    type: 'FETCH_MAKES',
+                    value: response.Results
+                }
+                dispatch(action)
+            })
+    }
+}
+
+export default fetchMakes;
